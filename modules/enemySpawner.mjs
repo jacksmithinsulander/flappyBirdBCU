29edb759efc4a12b17ff0a5949ec733e5deaf17c
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-11-29 20:47:15 +0100

    fixed repository

--- modules/enemySpawner.mjs
+++ modules/enemySpawner.mjs
@@ -1,47 +1,10 @@
-let hero = document.getElementById("hero");
-let game = document.getElementById("game");
+//anton funktion
 
 let bottom = 0;
 let left = 50;
-
-document.addEventListener("keyup", (e) => {
-    //console.log("e", e.key);
-
-    switch (e.key) {
-        case "w":
-        case "ArrowUp":
-           
-            bottom += 20;
-            hero.style.bottom = bottom + "px";
-        break;
-
-        case "s":
-        case "ArrowDown":
-            
-            bottom -= 20;
-            hero.style.bottom = bottom + "px";
-        break;
-
-        // TODO: BUGG i hitbox när vi rört oss i sidled
-
-        // case "d":
-        // case "ArrowRight":
-        //     left += 20;
-        //     hero.style.left = left + "px";
-        // break;
-
-        // case "a":
-        // case "ArrowLeft":
-        //     left -= 20;
-        //     hero.style.left = left + "px";
-        // break;
-
-    }
-})
-
 let enemyId = 0;
 
-function createEnemy() {
+export function createEnemy() {
     enemyId++
     let enemy = document.createElement("div");
     enemy.classList = "enemy";
@@ -85,5 +48,3 @@ function createEnemy() {
 
     game.appendChild(enemy);
 }
-
-createEnemy();
---
+++ script.js
@@ -0,0 +1,45 @@
+import { createEnemy } from './modules/enemySpawner.mjs';
+
+let hero = document.getElementById("hero");
+let game = document.getElementById("game");
+
+let bottom = 0;
+let left = 50;
+
+document.addEventListener("keyup", (e) => {
+    //console.log("e", e.key);
+
+    switch (e.key) {
+        case "w":
+        case "ArrowUp":
+           
+            bottom += 20;
+            hero.style.bottom = bottom + "px";
+        break;
+
+        case "s":
+        case "ArrowDown":
+            
+            bottom -= 20;
+            hero.style.bottom = bottom + "px";
+        break;
+
+        // TODO: BUGG i hitbox när vi rört oss i sidled
+
+        // case "d":
+        // case "ArrowRight":
+        //     left += 20;
+        //     hero.style.left = left + "px";
+        // break;
+
+        // case "a":
+        // case "ArrowLeft":
+        //     left -= 20;
+        //     hero.style.left = left + "px";
+        // break;
+
+    }
+})
+
+
+createEnemy();
