b54b03c3dc287727decc07a3d2cffdd1d1259925
Alex <mirelle.wallgren@medieinstitutet.se>
2022-11-29 19:39:23 +0100

    setup/skrev pseudokod

---
+++ gameOver.mjs
@@ -0,0 +1,12 @@
+export function(gameOver) {
+    if { //gubben dör 
+        // skapa en dom-manipulation-frame
+        // denna frame ska innehålla en knapp (hämta från login-uppg)
+        // både 'quit'- och 'try again'-knapp?
+
+        //knapparna behöver såklart eventliseners 
+        // profit 
+    } else {
+        console.log('du lever, nice');
+    }
+}
