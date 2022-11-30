b2361bb5b7721bb680a6b907e2a80be49e63db57
Alex <mirelle.wallgren@medieinstitutet.se>
2022-11-30 17:44:33 +0100

    :construction:

--- gameOver.mjs
+++ gameOver.mjs
@@ -1,12 +1,36 @@
-export function(gameOver) {
-    if { //gubben dör 
+
+    if  //gubben dör 
         // skapa en dom-manipulation-frame
         // denna frame ska innehålla en knapp (hämta från login-uppg)
         // både 'quit'- och 'try again'-knapp?
 
         //knapparna behöver såklart eventliseners 
         // profit 
-    } else {
-        console.log('du lever, nice');
-    }
+    else 
+       //  console.log('du lever, nice');
+    
+
+
+export function gameOver() {
+    gameOverBody.style.display = 'block';
+
+	//sätts in i eventListener
+	// startGame(); 
 }
+
+// hämtar in element
+const gameOverBody = document.getElementById('gameOverBody');
+const quitGameBtn = document.getElementById('quitGameBtn');
+
+function gameOver() {
+	let quitGameBtn = document.createElement("button");
+    quitGameBtn.innerText = "Quit";
+    demo.appendChild(logoutBtn);
+
+	
+}
+
+quitGameBtn.addEventListener('click'); {
+	gameOverBody.style.display = 'none'
+	
+};
