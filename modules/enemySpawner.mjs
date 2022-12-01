7461c4b526dbd9c704ac20f0bdfedebf9fc562db
jacksmithinsulander <666.jack.smith@protonmail.com>
2022-12-01 15:53:20 +0100

    new push, added enemyUnder

--- modules/enemySpawner.mjs
+++ modules/enemySpawner.mjs
@@ -1,5 +1,3 @@
-//anton funktion
-
 let bottom = 0;
 let left = 50;
 let enemyId = 0;
@@ -8,19 +6,28 @@ export function createEnemy() {
     enemyId++
     let enemy = document.createElement("div");
     enemy.classList = "enemy";
+    let enemyUnder = document.createElement("div");
+    enemyUnder.classList = "enemyUnder";
     let enemyLeft = 600;
-    let enemyBottom = Math.round(Math.round(Math.random() * 500)/10)*10;
+    let enemyBottom = Math.round(Math.round(Math.random() * 300)/10)*10;
     //console.log(enemyBottom);
     // Math.round(Math.floor(Math.random() * (500 - 1)/10)*10)
-
+	//console.log("random enemy bottom is", enemyBottom)
 
     enemy.style.left = enemyLeft + "px";
-    enemy.style.bottom = enemyBottom + "px";
+    enemy.style.height = enemyBottom + "px";
     enemy.id = enemyId;
 
+    enemyUnder.style.left = enemyLeft + "px";
+    enemyUnder.style.height = 420 - enemyBottom + "px";
+    enemyUnder.id = "enemyUnder id is" + enemyId;	
+
+//console.log("Height is", enemy.style.height)
+
     let move = setInterval(() => {
-        enemyLeft -= 50;
+        enemyLeft -= 30;
         enemy.style.left = enemyLeft + "px";
+	enemyUnder.style.left = enemyLeft + "px";
 
         if (enemyBottom > bottom && enemyBottom < bottom + 150 && enemyLeft === left) {
             console.log("HIT");
@@ -41,10 +48,12 @@ export function createEnemy() {
         if (enemyLeft <= 0) {
             clearInterval(move);
             enemy.remove();
+	enemyUnder.remove();
             createEnemy();
         }
 
     }, 50)
 
     game.appendChild(enemy);
+	game.appendChild(enemyUnder); 
 }
